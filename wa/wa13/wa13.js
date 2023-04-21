9238f0802e824aefa341f0a6118b0675595aa20b
stinkbugx <ashleycstafford@gmail.com>
2023-04-20 18:32:19 -0600

    WA13 Finished
    Added example.html
    Added example.js
    Updated wa13.html
    Updated wa13.js
    Updated wa13.css

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -1,101 +1,73 @@
-// If using canvas:
-// const canvas = document.querySelector('canvas');
-// const ctx = canvas.getContext('2d');
+//my stuff
+const volumeOutput = document.querySelector('.output');
+const butt1 = document.querySelector('#butt1');
+let buttAll = document.querySelector('.button');
+let buttonCount = 1;
+let currentText = "#butt" + buttonCount;
+let currentButt = document.querySelector(currentText);
+const buttonGroup = document.querySelector('.buttons');
+let submitButt = document.querySelector('#submit');
+let restartButt = document.querySelector('#restart');
+const congrats = document.querySelector('.congrats');
 
-// const width = canvas.width = window.innerWidth * 0.6;
-// const height = canvas.height = window.innerHeight * 0.8;
+currentButt.addEventListener('click', () => makeButton(buttonCount, currentButt));
 
-// function loop() {
-//     ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-//     ctx.fillRect(0, 0, width, height);
-  
-//     requestAnimationFrame(loop);
-// }
+submitButt.addEventListener('mouseover', swap);
+restartButt.addEventListener('mouseover', swap);
+submitButt.addEventListener('click', submit);
+restartButt.addEventListener('click', restart);
 
-// loop();
-const story = document.querySelector('#test1');
-const randomize = document.querySelector('.randomize');
-randomize.addEventListener('click', result);
-
-function result() {
-    story.textContent = "hi";
-    story.style.visibility = 'visible';
-}
-
-function check() {
-    console.log('test');
-}
-
-function submit() {
-    alert("your volume is now " + output.textContent);
-}
-
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
-    
-}
-
-function plus() {
-    if (outputInt < 100) {
-    outputInt +=1;
-    output.textContent = outputInt;
+function makeButton(count, current) {
+    let element = document.getElementById('butt' + (count+1))
+    if (element == null) {
+        volumeOutput.textContent = count - 1;
+        let newButton = document.createElement('button');
+        buttonGroup.appendChild(newButton);
+        newButton.textContent = count;
+        count +=1;
+        newButton.setAttribute('id', "butt" + count);
+        newButton.setAttribute('class', "button");
+        let text = "#butt" + count;
+        current = document.querySelector(text);
+        current.addEventListener('click', () => makeButton(count, current));
+        buttonCount = count;
     }
 }
 
-function random() {
-    outputInt = randomNumber(0, 100);
-    output.textContent = outputInt;
+function swap(){
+    if (submitButt.textContent == "Restart"){
+        submitButt.textContent = "Submit";
+        restartButt.textContent = "Restart";
+    } else {
+        submitButt.textContent = "Restart";
+        restartButt.textContent = "Submit";
+    }
 }
 
-function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
+function submit() {
+    // if (submitButt.textContent == "Restart"){
+    //     alert("restart test");
+    // } else {
+    //     alert("submit test");
+    // }
 }
 
-
-
-const output = document.querySelector('.output');
-let outputInt = parseInt(output.textContent);
-console.log(outputInt);
-
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
-
-
-var slider = document.getElementById("myRange");
-slider.addEventListener('click', update);
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', slideSubmit);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
-}
-
-function slideSubmit() {
-    alert("your volume is now " + sliderOutput.textContent);
+function restart() {
+    if (restartButt.textContent == "Restart"){
+        let element = document.getElementById('butt1');
+        console.log("restart ran");
+        for (i=2; i<=buttonCount; i++){
+            console.log("for loop: " + i);
+            element = document.getElementById('butt' + i);
+            if (element !== null) {
+                console.log("if entered");
+                element.remove();
+            }
+        }
+        buttonCount = 1;
+        volumeOutput.textContent = 0;
+    } else {      
+        alert("Sorry Anthony");
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }
 }
