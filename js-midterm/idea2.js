5e1dc900468597f001efa95a2506592fe9babe57
stinkbugx <ashleycstafford@gmail.com>
2023-07-04 09:33:13 -0600

    Unity code reference

--- js-midterm/idea2.js
+++ js-midterm/idea2.js
@@ -1,13 +1,14 @@
 const start = document.querySelector('#start');
 const buttGrid = document.querySelector('.button-grid');
 const instruct = document.querySelector('.instructions');
-// const testArray = document.querySelector('#test');
+const testArray = document.querySelector('#test');
 //const testWin = document.querySelector('#win');
 let ticValues = [];
 let turn = ""; //alternates between X and O
+let turnCount = 0;
 
 start.addEventListener('click', gameStart);
-// testArray.addEventListener('click', testVal);
+testArray.addEventListener('click', testVal);
 //testWin.addEventListener('click', checkWin);
 buttGrid.addEventListener('click', winCondition);
 
@@ -15,6 +16,11 @@ buttGrid.addEventListener('click', winCondition);
 function gameStart() {
     instruct.textContent = "Click any space to play, X's go first";
     winState = false;
+    turnCount = 0;
+    move1 = "";
+    move2 = "";
+    move3 = "";
+    move4 = "";
     turn = "X"; //X's go first
     for (i = 0; i < 9; i++){ //for all array values
         if (ticValues.length == 9){ //replaces values if array already full
@@ -58,19 +64,41 @@ function gameStart() {
     butt9.addEventListener('click', change9);
 }
 
+let currentMove = "";
+
 //yes individual change functions,, I was not willing to teach myself makng functions with variables for names
 function change1() {
     // console.log("change1 ran");
     if (ticValues[0] == ""){
-        if (turn == "X"){ //X's turn
-            butt1.textContent = "X"; 
-            turn = "O"; 
-            ticValues[0] = "X";
-        } else {
-            butt1.textContent = "O";
-            turn = "X";
-            ticValues[0] = "O";
+        butt1.textContent = "X"; 
+        turn = "O"; 
+        ticValues[0] = "X";
+        // if (turn == "X"){ //X's turn
+        //     butt1.textContent = "X"; 
+        //     turn = "O"; 
+        //     ticValues[0] = "X";
+        // } else {
+        //     butt1.textContent = "O";
+        //     turn = "X";
+        //     ticValues[0] = "O";
+        // }
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "tl";
+                break;
+            case 2:
+                move2 = "tl";
+                break;
+            case 3:
+                move3 = "tl";
+                break;
+            case 4:
+                move4 = "tl";
+                break;
         }
+        oTurn();
+        
     } else {
         console.log("No reclicks >:(");
     }
@@ -80,15 +108,25 @@ function change1() {
 function change2() {
     // console.log("change1 ran");
     if (ticValues[1] == ""){
-        if (turn == "X"){ //X's turn
-            butt2.textContent = "X"; 
-            turn = "O"; 
-            ticValues[1] = "X";
-        } else {
-            butt2.textContent = "O";
-            turn = "X";
-            ticValues[1] = "O";
+        butt2.textContent = "X"; 
+        turn = "O"; 
+        ticValues[1] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "ts";
+                break;
+            case 2:
+                move2 = "ts";
+                break;
+            case 3:
+                move3 = "ts";
+                break;
+            case 4:
+                move4 = "ts";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
@@ -97,15 +135,25 @@ function change2() {
 function change3() {
     // console.log("change1 ran");
     if (ticValues[2] == ""){
-        if (turn == "X"){ //X's turn
-            butt3.textContent = "X"; 
-            turn = "O"; 
-            ticValues[2] = "X";
-        } else {
-            butt3.textContent = "O";
-            turn = "X";
-            ticValues[2] = "O";
+        butt3.textContent = "X"; 
+        turn = "O"; 
+        ticValues[2] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "tr";
+                break;
+            case 2:
+                move2 = "tr";
+                break;
+            case 3:
+                move3 = "tr";
+                break;
+            case 4:
+                move4 = "tr";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
@@ -114,15 +162,25 @@ function change3() {
 function change4() {
     // console.log("change1 ran");
     if (ticValues[3] == ""){
-        if (turn == "X"){ //X's turn
-            butt4.textContent = "X"; 
-            turn = "O"; 
-            ticValues[3] = "X";
-        } else {
-            butt4.textContent = "O";
-            turn = "X";
-            ticValues[3] = "O";
+        butt4.textContent = "X"; 
+        turn = "O"; 
+        ticValues[3] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "ls";
+                break;
+            case 2:
+                move2 = "ls";
+                break;
+            case 3:
+                move3 = "ls";
+                break;
+            case 4:
+                move4 = "ls";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
@@ -131,15 +189,25 @@ function change4() {
 function change5() {
     // console.log("change1 ran");
     if (ticValues[4] == ""){
-        if (turn == "X"){ //X's turn
-            butt5.textContent = "X"; 
-            turn = "O"; 
-            ticValues[4] = "X";
-        } else {
-            butt5.textContent = "O";
-            turn = "X";
-            ticValues[4] = "O";
+        butt5.textContent = "X"; 
+        turn = "O"; 
+        ticValues[4] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "c";
+                break;
+            case 2:
+                move2 = "c";
+                break;
+            case 3:
+                move3 = "c";
+                break;
+            case 4:
+                move4 = "c";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
@@ -148,15 +216,25 @@ function change5() {
 function change6() {
     // console.log("change1 ran");
     if (ticValues[5] == ""){
-        if (turn == "X"){ //X's turn
-            butt6.textContent = "X"; 
-            turn = "O"; 
-            ticValues[5] = "X";
-        } else {
-            butt6.textContent = "O";
-            turn = "X";
-            ticValues[5] = "O";
+        butt6.textContent = "X"; 
+        turn = "O"; 
+        ticValues[5] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "rs";
+                break;
+            case 2:
+                move2 = "rs";
+                break;
+            case 3:
+                move3 = "rs";
+                break;
+            case 4:
+                move4 = "rs";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
@@ -165,15 +243,25 @@ function change6() {
 function change7() {
     // console.log("change1 ran");
     if (ticValues[6] == ""){
-        if (turn == "X"){ //X's turn
-            butt7.textContent = "X"; 
-            turn = "O"; 
-            ticValues[6] = "X";
-        } else {
-            butt7.textContent = "O";
-            turn = "X";
-            ticValues[6] = "O";
+        butt7.textContent = "X"; 
+        turn = "O"; 
+        ticValues[6] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "bl";
+                break;
+            case 2:
+                move2 = "bl";
+                break;
+            case 3:
+                move3 = "bl";
+                break;
+            case 4:
+                move4 = "bl";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
@@ -182,15 +270,25 @@ function change7() {
 function change8() {
     // console.log("change1 ran");
     if (ticValues[7] == ""){
-        if (turn == "X"){ //X's turn
-            butt8.textContent = "X"; 
-            turn = "O"; 
-            ticValues[7] = "X";
-        } else {
-            butt8.textContent = "O";
-            turn = "X";
-            ticValues[7] = "O";
+        butt8.textContent = "X"; 
+        turn = "O"; 
+        ticValues[7] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "bs";
+                break;
+            case 2:
+                move2 = "bs";
+                break;
+            case 3:
+                move3 = "bs";
+                break;
+            case 4:
+                move4 = "bs";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
@@ -199,40 +297,50 @@ function change8() {
 function change9() {
     // console.log("change1 ran");
     if (ticValues[8] == ""){
-        if (turn == "X"){ //X's turn
-            butt9.textContent = "X"; 
-            turn = "O"; 
-            ticValues[8] = "X";
-        } else {
-            butt9.textContent = "O";
-            turn = "X";
-            ticValues[8] = "O";
+        butt9.textContent = "X"; 
+        turn = "O"; 
+        ticValues[8] = "X";
+        turnCount++;
+        switch (turnCount){
+            case 1:
+                move1 = "br";
+                break;
+            case 2:
+                move2 = "br";
+                break;
+            case 3:
+                move3 = "br";
+                break;
+            case 4:
+                move4 = "br";
+                break;
         }
+        oTurn();
     } else {
         console.log("No reclicks >:(");
     }
 }
 
-// function testVal() { //just for me to see what the computer believes the game to be
-//     if (ticValues.length == 0){ //if array is empty
-//         console.log("Empty array"); 
-//     } else {
-//         // for (i = 0; i < ticValues.length; i++){ //prints current array values
-//         //     console.log(ticValues[i]);
-//         // }
-//         for (j = 0; j < 3; j++){ //makes a board with the values
-//             let line = "";
-//             for (i = (j*3); i < ((j*3)+3); i++){
-//                 if (ticValues[i] == "") {
-//                     line = line + "#";
-//                 } else {
-//                     line = line + ticValues[i];
-//                 }
-//             }
-//             console.log(line);
-//         }
-//     }
-// }
+function testVal() { //just for me to see what the computer believes the game to be
+    if (ticValues.length == 0){ //if array is empty
+        console.log("Empty array"); 
+    } else {
+        // for (i = 0; i < ticValues.length; i++){ //prints current array values
+        //     console.log(ticValues[i]);
+        // }
+        for (j = 0; j < 3; j++){ //makes a board with the values
+            let line = "";
+            for (i = (j*3); i < ((j*3)+3); i++){
+                if (ticValues[i] == "") {
+                    line = line + "#";
+                } else {
+                    line = line + ticValues[i];
+                }
+            }
+            console.log(line);
+        }
+    }
+}
 
 /* 
 012
@@ -336,3 +444,69 @@ function winCondition() {
 //         console.log("no win!");
 //     }
 // }
+
+/* 
+012
+345
+678*/
+
+let move1 = ""; //center, corner, or side
+let move2 = ""; 
+let move3 = "";
+let move4 = "";
+
+function oTurn() {
+    console.log(turnCount);
+    if (turnCount == 1) { //first turn
+        if (move1 == "c") { // if center spot is taken
+            ticValues[2] = "O"; //takes the tr corner
+            butt3.textContent = "O";
+            move1 = "center"
+        } else if (move1 == "tl" || move1 == "tr" || move1 == "bl" || move1 == "br") { //if a corner is taken
+            ticValues[4] = "O"; //takes the center
+            butt5.textContent = "O";
+            move1 = "corner"
+        } else { //for any other spot
+            ticValues[4] = "O"; //takes the center
+            butt5.textContent = "O";
+            move1 = "side"
+        }
+    } else if (turnCount == 2) {
+        if (move1 == "center") { //if the first move was in the center
+            if (ticValues[0] == "X") { //tl corner
+                ticValues[8] = "O"; //blocks row
+                butt9.textContent = "O";
+                move2 = "corner"
+            } else if (ticValues[8] == "X") { //br corner
+                ticValues[0] = "O"; //blocks row
+                butt1.textContent = "O";
+                move2 = "corner"
+            } else if (ticValues[6] == "X") { //bl corner
+                ticValues[5] = "O"; //takes side
+                butt6.textContent = "O";
+                move2 = "corner"
+            } else if (ticValues[1] == "X") { //t side
+                ticValues[7] = "O"; //blocks row
+                butt8.textContent = "O";
+                move2 = "side"
+            } else if (ticValues[3] == "X") { //l side
+                ticValues[5] = "O"; //blocks row
+                butt6.textContent = "O";
+                move2 = "side"
+            } else if (ticValues[5] == "X") { //r side
+                ticValues[3] = "O"; //blocks row
+                butt4.textContent = "O";
+                move2 = "side"
+            } else if (ticValues[7] == "X") { //b side
+                ticValues[1] = "O"; //blocks row
+                butt2.textContent = "O";
+                move2 = "side"
+            }
+        } else if (move1 == "corner") { //if the first move was in a corner (O took center)
+            if (ticValues[])
+        } else { //if the first move was on a side (O took center)
+            d
+        }
+    } else if (turnCount == 3) {
+    }
+}
